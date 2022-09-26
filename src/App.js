4893ce1d4471b77a742fdcaae5328a48be631fae
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-09-26 15:17:11 -0400

    created dynamic coordinate change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
+import { getPlacesData } from "./api/index";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(coordinates, bounds);
+    getPlacesData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, [coordinates, bounds]);
   return (
     <>
       <CssBaseline />
@@ -15,7 +35,11 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,13 +5,12 @@ import LocationOnOutlinesIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
 
 import useStyles from "./styles";
+import { getPlacesData } from "../../api";
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const classes = useStyles();
   //set to false if width of device is larger than 600px
   const isMobile = useMediaQuery("(min-width:600px)");
-  const coordinates = { lat: 0, lng: 0 };
-
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
@@ -21,8 +20,11 @@ const Map = () => {
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={""}
-        onChange={""}
-        onChildClick={""}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
       ></GoogleMapReact>
     </div>
   );
